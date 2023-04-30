d3343dd21d9c0e8713e445fa2288c2566e02c7b6
AndersEnrico <129499305+AndersEnrico@users.noreply.github.com>
2023-04-30 22:39:50 +0200

    Update Karakterafrundningsfunktion.py
    har fået den til at virke med matricen, ved at ændre det sidste elif statement med et else

--- Karakterafrundningsfunktion.py
+++ Karakterafrundningsfunktion.py
@@ -1,21 +1,18 @@
-import numpy as np
 def roundGrade(grades):
     gradesRounded = []
     for en_grade in grades:
-        if en_grade<-1.5:   
+        if en_grade < -1.5:   
             gradesRounded.append(-3)
-        elif en_grade<1:
-            gradesRounded.append(00)
-        elif en_grade<3:
+        elif en_grade < 1:
+            gradesRounded.append(0)
+        elif en_grade < 3:
             gradesRounded.append(2)
-        elif en_grade<5.5:
+        elif en_grade < 5.5:
             gradesRounded.append(4)
-        elif en_grade<8.5:
+        elif en_grade < 8.5:
             gradesRounded.append(7)
-        elif en_grade<11:
+        elif en_grade < 11:
             gradesRounded.append(10)
-        elif en_grade>11:
+        else:
             gradesRounded.append(12)
-            
     return gradesRounded
-   
