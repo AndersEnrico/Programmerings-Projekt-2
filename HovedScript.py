8db59cef1a5d99395d508d21d91b8ab31275312a
AndersEnrico <129499305+AndersEnrico@users.noreply.github.com>
2023-05-02 00:13:05 +0200

    Update HovedScript.py

--- HovedScript.py
+++ HovedScript.py
@@ -110,6 +110,93 @@ while True:
 
 
 
+#Denne kode er skrevet af Anders Enrico Krog Petersen, s224076.
+
+# 3. Generer Plots
+                elif choice == 3:
+                    print(gradesPlot(gradesMatrix))
+
+#------------------------------------------------------------------------------
+
+# 4. Vis karakterliste
+
+
+#------------------Viser afrundet karakterer fra hver opgave-------------------
+
+
+                elif choice == 4:
+                    print('\x1b[6;30;42m' + f'Herunder ses karakterene for alle {assignCount} opgaver:' + '\x1b[0m')
+
+# Afrunder karakterende til 7-trinsskalen og printer dem
+                    for i in range(len(gradesMatrix)):
+                        for j in range(len(gradesMatrix[i])):
+                            gradesMatrix[i][j] = roundGrade([gradesMatrix[i][j]])[0]
+                    
+# Laver en liste uden studienumre og en kun med studienumre.                 
+                    noStudentNumber = np.delete(dataMatrix,0,1)
+                    studentNumber = np.delete(dataMatrix,slice(1,len(dataMatrix)),1).tolist()
+
+#Laver en liste kun med elevernes navne (ikke alfabetisk rækkefølge).
+                    nonSortedNames = np.delete(noStudentNumber,slice(1,len(gradesMatrix)),1).tolist()
+                    
+# Indsætter studienumrene igen i listen 'nonSortedNames'.
+                    for i, element in enumerate(nonSortedNames):
+                        element.append(studentNumber[i])
+                    
+# Indsætter elevernes endelige karakter i listen 'names'.
+                    for i, element in enumerate(nonSortedNames):
+                        element.append(gradesMatrix[i])
+
+# Vi konvertere nu numpy array'en til en normal list, så der ikke står "dtype=object" når det printes
+                    for i, element in enumerate(nonSortedNames):
+                        for j in range(len(element)):
+                            if isinstance(element[j], np.ndarray):
+                                element[j] = element[j].tolist()
+                    
+                    nonSortedNames = [[item[0], item[1][0], item[2]] for item in nonSortedNames]
+                    
+                    
+                    print(nonSortedNames)
+                    
+                    
+#-----------------Viser elevernes endeligere karakterer------------------------
+
+
+# Sorterer matricen i alfabetisk rækkefølge efter deres navne
+                    sorteretMatrix = np.sort(noStudentNumber, axis=0)
+                    
+# Laver en liste uden navne i og finder elevernes endelige karakter
+                    noName = np.delete(sorteretMatrix,0,1)
+                    final = computeFinalGrades(noName)
+
+# Laver en numpy array kun med elevernes navne og laves om til en normal liste
+                    names = np.delete(sorteretMatrix,slice(1,len(sorteretMatrix)),1).tolist()
+                    
+# Indsætter studienumrene igen i listen 'names'.
+                    for i, element in enumerate(names):
+                        element.append(studentNumber[i])
+                    
+# Indsætter elevernes endelige karakter i listen 'names'.
+                    for i, element in enumerate(names):
+                        element.append(final[i])
+                    
+# fjerner de firkantede paranteser, som var rundt om studienummeret
+                    names = [[item[0], item[1][0], item[2]] for item in names]
+                    
+                    print('\x1b[6;30;42m' + 'Herunder ses elevernes endeligere karakterer:' + '\x1b[0m')
+                    print(names)
+
+#------------------------------------------------------------------------------
+
+# 5. Afslut
+                elif choice == 5:
+                
+                    break
+                    
+                    
+                        
+            
+
 # Hvis filen ikke eksistere giver den fejlbesked og sender retur til hoved menu.        
         elif check_file == False:
             print('\x1b[0;30;41m' + f"{filNavn} eksistere ikke i mappen, eller er der tastefejl, prøv igen." + '\x1b[0m')
