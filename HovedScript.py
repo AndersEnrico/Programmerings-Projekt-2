36f99473ee4a2bdfc48e569805b79311e95aa0ce
AndersEnrico <129499305+AndersEnrico@users.noreply.github.com>
2023-05-01 20:23:33 +0200

    Update HovedScript.py

--- HovedScript.py
+++ HovedScript.py
@@ -38,34 +38,34 @@ while True:
 # Checker om filen findes        
         check_file = os.path.isfile(filNavn)
         
-# Laver csv filen om til en NxM matrice, hvor N er antale elever og M er antal opgaver,
-# Og indeholder kun karakterer
-        dataFile = pd.read_csv(filNavn)
-        gradesMatrix = np.array(dataFile)
-        gradesMatrix = np.delete(gradesMatrix,slice(0,2),1)
-        
-# Antal elever og opgaver.
-        studentCount = len(gradesMatrix[:,0])
-        assignCount = len(gradesMatrix[0,:])
-        
         if check_file == True:
             print('\x1b[6;30;42m' + f'{filNavn} er indlæst korrekt.' + '\x1b[0m')
-            print('\x1b[6;30;42m' + f'Der er indlæst {studentCount} elever og {assignCount} opgaver' + '\x1b[0m')
+            #print('\x1b[6;30;42m' + f'Der er indlæst {studentCount} elever og {assignCount} opgaver' + '\x1b[0m')
            
             while True:
+                
+# Laver csv filen om til en matrice og fjerner alt andet end karakterer
+                dataFile = pd.read_csv(filNavn)
+                dataMatrix = np.array(dataFile)
+                gradesMatrix = np.delete(dataMatrix,slice(0,2),1)
+                
+# Antal elever og opgaver
+                studentCount = len(gradesMatrix[:,0])
+                assignCount = len(gradesMatrix[0,:])
+                print('\x1b[6;30;42m' + f'Der er indlæst {studentCount} elever og {assignCount} opgaver' + '\x1b[0m')
 # ------------------------------------------------------------------
 
 # Denne kode er skrevet af Kasper Mejer Lærche Laursen, s224196
-                 choice = displayMenu(menuItems)
+                choice = displayMenu(menuItems)
 # 2. Filtrer data
-                 if choice == 1:
+                if choice == 1:
 # under choice 2 laves en ny meny
                      while True:
                          
                         choice2 = displayMenu(menuItems2)
                         
                 
-                 elif choice2 == 2:
+                elif choice2 == 2:
                      
                      break
             
